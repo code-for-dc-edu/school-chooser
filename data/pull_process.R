cda52ae18cd8d76ef01dec1cbc300f2db5c64f0b
Harlan Harris <harlan@harris.name>
2014-02-04 16:10:03 -0500

    a bunch of work on getting culture data loaded

--- data/pull_process.R
+++ data/pull_process.R
@@ -36,10 +36,10 @@ ret <- with(ov1, list(
 
 lapply(pf1$profile$sections, function(x) x$id)
 
-getValue <- function(arrayOfKeyVals, keyname, keyval) {
+getValue <- function(arrayOfKeyVals, keyname, keyval, result_numeric=FALSE) {
     for (kv in arrayOfKeyVals) {
         if (kv$key[keyname] == keyval) {
-            return(as.numeric(kv$val))
+            return(if (result_numeric) as.numeric(kv$val) else kv$val)
         }
     }
     return(NA)
@@ -51,12 +51,12 @@ makeDiversityBlock <- function(prof) {
     
     # the keys that we care about are:
     # grade=='All', year=='2012', and subgroup==?
-    ret <- list(val=c(asian=getValue(sect$data, "subgroup", "Asian"),
-                         africanAmerican=getValue(sect$data, "subgroup", "African American"),
-                         multiracial=getValue(sect$data, "subgroup", "Multi Racial"),
+    ret <- list(val=c(asian=getValue(sect$data, "subgroup", "Asian", result_numeric=TRUE),
+                         africanAmerican=getValue(sect$data, "subgroup", "African American", result_numeric=TRUE),
+                         multiracial=getValue(sect$data, "subgroup", "Multi Racial", result_numeric=TRUE),
                          hawaiianPacificIslander=0,
-                         white=getValue(sect$data, "subgroup", "White"),
-                         hispanic=getValue(sect$data, "subgroup", "Hispanic"),
+                         white=getValue(sect$data, "subgroup", "White", result_numeric=TRUE),
+                         hispanic=getValue(sect$data, "subgroup", "Hispanic", result_numeric=TRUE),
                          americanIndianAlaskaNative=0),
                 sd=0)
     procDiversityBlock(ret)
@@ -70,25 +70,69 @@ procDiversityBlock <- function(ll) {
 }
 makeDiversityBlock(pf1)
 
-makeCultureBlock <- function(prof) {
-    # confirm that section 2 is the enrollment
-    sect <- prof$profile$sections[[2]]
-    stopifnot(sect$id == 'enrollment')
-    
-    # the keys that we care about are:
-    # grade=='All', year=='2012', and subgroup==?
-    ret <- list(val=c(asian=getValue(sect$data, "subgroup", "Asian"),
-                      africanAmerican=getValue(sect$data, "subgroup", "African American"),
-                      multiracial=getValue(sect$data, "subgroup", "Multi Racial"),
-                      hawaiianPacificIslander=0,
-                      white=getValue(sect$data, "subgroup", "White"),
-                      hispanic=getValue(sect$data, "subgroup", "Hispanic"),
-                      americanIndianAlaskaNative=0),
-                sd=0)
-    procDiversityBlock(ret)
-}
-makeCultureBlock(pf1)
+# culture is based on withdrawal, suspension, attendance, unexclused absenses
+# first, get those numbers from one school
+# then, separately, normalize all schools
 
+n20 <- function(x) if (is.null(x)) 0 else x
+n2na <- function(x) if (is.null(x)) NA else x
+
+makeCultureDF <- function(prof) {
+    # confirm that we have the right sections
+    tru <- prof$profile$sections[[6]]
+    att <- prof$profile$sections[[7]]
+    sus <- prof$profile$sections[[8]]
+    exp <- prof$profile$sections[[9]]
+    myew <- prof$profile$sections[[10]]
+    stopifnot(tru$id == 'unexcused_absences')
+    stopifnot(att$id == 'attendance'); stopifnot(sus$id == 'suspensions')
+    stopifnot(exp$id == 'expulsions'); stopifnot(myew$id == 'mid_year_entry_and_withdrawal')
+    
+    # extract and put into a DF for later processing
+    #myew_val <- getValue(myew$data, "month", 5)
+    ret <- data.frame(in_seat_attendance=try_default(n2na(att$data[[1]]$val$in_seat_attendance), NA),
+                      state_in_seat_attendance=try_default(n2na(att$data[[1]]$val$state_in_seat_attendance), NA),
+                      suspended_1=try_default(sus$data[[1]]$val$suspended_1, NA),
+                      state_suspended_1=try_default(sus$data[[1]]$val$state_suspended_1, NA),
+                      expulsions=try_default(exp$data[[1]]$val$expulsions, NA),
+                      state_expulsions=try_default(exp$data[[1]]$val$state_expulsions, NA),
+                      myew=try_default(getValue(myew$data, "month", 5)$net_cumulative, NA),
+                      truancy=n20(try_default(tru$data[[1]]$val$`16-25_days`, NULL)) + 
+                          n20(try_default(tru$data[[1]]$val$more_than_25_days, NULL)),
+                      truancy_state=n20(try_default(tru$data[[1]]$val$`state_16-25_days`, NULL)) + 
+                          n20(try_default(tru$data[[1]]$val$state_more_than_25_days, NULL))
+                      )
+    ret
+}
+makeCultureDF(pf1)
+
+# TODO: with everyone's data, do appropriate normalization
+
+# hammer the server to get all the report card data, then loop through to 
+# build the appropriate structures
+
+sc_fmt <- "http://learndc.org/data/overview/school_%04d_overview.JSON"
+overviews <- llply(school_codes, function(sc) {
+    sc_json <- jsonlite::fromJSON(sprintf(sc_fmt, sc))
+    with(sc_json, list(
+        code=org_code,
+        name=org_name,
+        grades=grades_serviced,
+        address=address,
+        website=website,
+        profile=external_report_card,
+        charter=charter
+    ))
+}, .progress='time')
+
+pf_fmt <- "http://learndc.org/data/profile/school_%04d.JSON"
+profiles <- llply(school_codes, function(pf) {
+    pf_json <- jsonlite::fromJSON(sprintf(pf_fmt, pf), simplifyDataFrame=FALSE)
+    pf_json$code <- pf
+    pf_json
+}, .progress='time')
+ 
+culture_df <- ldply(profiles, function(pf) cbind(makeCultureDF(pf), code=pf$code))
 
 ##############################
 # Get the PCSB Equity data set
